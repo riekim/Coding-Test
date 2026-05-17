0526a0af7e0d1c207abc6226f41cb5dd8e7f0f86
kimire <rladlfp888@gmail.com>
2026-05-17 21:03:02 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.02 ms, Memory: 9.27 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
@@ -0,0 +1,14 @@
+def solution(a, b):
+    answer = 0
+    
+    ab = int(str(a) + str(b))
+    ba = int(str(b) + str(a))
+    
+    if ab > ba:
+        answer = ab
+    elif ab < ba:
+        answer = ba
+    else:
+        answer = ab
+        
+    return answer
